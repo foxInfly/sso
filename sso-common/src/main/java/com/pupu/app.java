3cef89307274aae264f7d06ff5a852473c9d104f
lipu <lipu@zgzhongnan.com>
2020-08-21 15:50:54 +0800

    sso-common

---
+++ sso-common/src/main/java/com/pupu/app.java
@@ -0,0 +1,8 @@
+package com.pupu;
+
+/**
+ * @author lipu
+ * @since 2020-08-21 15:49:58
+ */
+public class app {
+}
---
+++ sso-common/src/test/java/com/pupu/test.java
@@ -0,0 +1,8 @@
+package com.pupu;
+
+/**
+ * @author lipu
+ * @since 2020-08-21 15:50:25
+ */
+public class test {
+}

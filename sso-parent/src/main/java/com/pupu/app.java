7f11041aa226d90edc66d49eaea3122e4774a6b9
lipu <lipu@zgzhongnan.com>
2020-08-21 15:45:49 +0800

    Initial commit

---
+++ sso-parent/src/main/java/com/pupu/app.java
@@ -0,0 +1,8 @@
+package com.pupu;
+
+/**
+ * @author lipu
+ * @since 2020-08-21 15:02:05
+ */
+public class app {
+}
---
+++ sso-parent/src/test/java/com/pupu/test.java
@@ -0,0 +1,8 @@
+package com.pupu;
+
+/**
+ * @author lipu
+ * @since 2020-08-21 15:02:39
+ */
+public class test {
+}

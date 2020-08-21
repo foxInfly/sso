18b27f3ace79c6d6978094e4913b34c81691e46f
lipu <lipu@zgzhongnan.com>
2020-08-21 18:07:48 +0800

    sso-common-02

--- sso-common/src/main/java/com/pupu/app.java
+++ sso-common/src/main/java/com/pupu/app.java
@@ -1,8 +1,14 @@
 package com.pupu;
 
+import org.apache.http.client.methods.HttpGet;
+
 /**
  * @author lipu
  * @since 2020-08-21 15:49:58
  */
 public class app {
+    public static void main(String[] args) {
+        String a = "wwww";
+        HttpGet httpGet = new HttpGet("");
+    }
 }
